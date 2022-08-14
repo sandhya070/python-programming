728844f072cd4f885f12157bcb7232c59fbb8ae4
shallu99 <shreyankm262@gmail.com>
2022-08-14 12:10:02 +0530

    first comit

---
+++ numbers in python.py
@@ -0,0 +1,30 @@
+tumk_bang=70
+bang_mys=180
+total_distance=tumk_bang+bang_mys
+print(total_distance)
+mph=80
+time=total_distance/mph
+print(time)
+print(round(time,1))
+#some moe excercise#
+number=2+6
+print(number)
+#sum of numbers#
+num1=5+2+30+50**5/50
+num2=50+65-20*89/55
+num3=800+200+300+5*655
+print(num1)
+print(num2)
+print(num3)
+total=(num1+num2+num3)
+print(total)
+print(round(total,5))
+#to store floating numbers#
+one_equation=6+2/15
+print(one_equation)
+print(round(one_equation,4))
+#xx#
+round=(6+555,2)
+print(round,3)
+
+
---
+++ variables in python.py
@@ -0,0 +1,17 @@
+gas=150
+rent=5000
+waterbill=400
+sum=gas+rent+waterbill
+print(sum)
+
+#variable and python keywords#
+
+item1= "banana"
+item2= "apple"
+item3= "mango"
+total=item1+item2+item3
+print(total)
+
+
+
+
