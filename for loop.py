e2fbe4200d96cbe0031d96f13e1b7dd5124e4317
shallu99 <shreyankm262@gmail.com>
2022-08-22 20:29:29 +0530

    first comit.

---
+++ for loop.py
@@ -0,0 +1,22 @@
+exp = [1200 , 4000 , 5000  ]
+total = 0
+for item in exp :
+   total = total + item
+print(total)
+
+#
+
+for x in range (1, 101):
+    print(x)
+#
+for x in range (1 , 10):
+     print(x*x)
+# to print indivisual expense and total expense
+
+exp = [ 1000 , 2000 , 3000 , 4000 ]
+total = 0
+for i in range (len(exp)):
+  print('month:' , ( i +1), 'expense: ' , exp[i])
+  total = total+ exp[i]
+print("total expense is :", total)
+
