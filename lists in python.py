4fa89fbffb285a3ca685c138474d737afc3ec77e
shallu99 <shreyankm262@gmail.com>
2022-08-22 13:04:37 +0530

    .

---
+++ lists in python.py
@@ -0,0 +1,9 @@
+
+item1="stearing cover"
+item2="break pads"
+item3="engine oil"
+item4="clutch wire"
+items=["stearing cover","break pads","engine oil","clutch wire"]
+print(items)
+print(items[2])
+print("the third item in the itesm list is",item2)
