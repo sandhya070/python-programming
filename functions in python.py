cafd62c5bc0453a72a3ac3799d66daa9bd2deaa9
shallu99 <shreyankm262@gmail.com>
2022-09-09 19:49:21 +0530

    first comit.

---
+++ functions in python.py
@@ -0,0 +1,18 @@
+tom_exp_list = [ 2100, 3400, 3500]
+joe_exp_list = [ 200, 500, 700]
+
+
+total = 0
+for item in tom_exp_list:
+    total =total+item
+    print("tom total expenses :",total)
+
+    total = 0
+    for item in joe_exp_list:
+        total = total+item
+        print("joe total expenses :", total)
+
+
+
+
+
